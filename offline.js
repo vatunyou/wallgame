9b6f8b8c88cc62e78ac2f652b706984f65077c81
vatunyou <42827005+vatunyou@users.noreply.github.com>
2018-09-13 15:09:21 +0700

    wallgame

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1536826007,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/p1-sheet0.png",
+		"images/p2-sheet0.png",
+		"images/hero-sheet0.png",
+		"images/star-sheet0.png",
+		"images/restart-sheet0.png",
+		"images/spawnpoint-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
